2836a90da34db0f36ebc085dad9a6cdd779cb303
AlexTGarcia020 <129820523+AlexTGarcia020@users.noreply.github.com>
2023-12-27 11:23:41 -0300

    Cambios generales

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+/* import './App.css' */
+
+function Navbar() {
+
+  return (
+    <>
+     <h1>Hola</h1>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+/* import './App.css' */
+
+function Main() {
+
+  return (
+    <>
+    <h1>Main</h1>
+    </>
+  )
+}
+
+export default Main
