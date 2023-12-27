d3dd91865b61d63b15a88b6ab8c7bb0430596aa1
AlexTGarcia020 <129820523+AlexTGarcia020@users.noreply.github.com>
2023-12-27 12:28:38 -0300

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-
-import './App.css'
-
-import Navbar from './components/Navbar'
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
-    <>
-     <Navbar/>
-    </>
-  )
+    <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+      <Navbar toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
+      <div className="content">
+        {/* Contenido del resto de tu aplicación */}
+        {/* Por ejemplo: */}
+        <h1>Bienvenido a mi aplicación</h1>
+        </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-/* import './App.css' */
+import React from 'react';
+import "../css/Navbar.css";
 
-function Navbar() {
+function Navbar({ toggleDarkMode, isDarkMode }) {
+
+const handleToggleClick = () => {
+ toggleDarkMode(!isDarkMode);
+ };
 
   return (
-    <>
-     <h1>Hola</h1>
-    </>
-  )
+    <nav className="navbar">
+      <div className="brand">Tu Logo</div>
+      <div className="container">
+      <label class="switch">
+        <input type="checkbox"  onClick={toggleDarkMode}/>
+        <span class="slider"></span>
+        </label>
+    </div>
+      {/* Otros elementos de navegación */}
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
