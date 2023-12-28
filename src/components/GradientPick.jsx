a1a949c7826a01c6e15bdddb47bf75593ce28c1a
AlexTGarcia020 <129820523+AlexTGarcia020@users.noreply.github.com>
2023-12-28 17:25:04 -0300

    Err

--- src/components/GradientPick.jsx
+++ src/components/GradientPick.jsx
@@ -5,10 +5,34 @@ import "../css/Gradient.css"
 function GradientPick() {
   const [backgroundGradient, setBackgroundGradient] = useState('linear-gradient(90deg, rgba(96,93,93,1) 0%, rgba(255,255,255,1) 100%)');
 
+  const handleColorChange = (color) => {
+    setBackgroundGradient(color);
+  };
+
+  /* const customLocales = {
+    CONTROLS: {
+      SOLID: 'Solido',
+      GRADIENT: 'Gradiente',
+    },
+  } */
+
   return (
     <div className='div-Gradient'>
      <div className='card p-3 rounded shadow'>
-     <ColorPicker/>
+     <ColorPicker
+       /*  locales={customLocales} */
+        value={backgroundGradient}
+        onChange={handleColorChange}
+        hideColorTypeBtns={false}
+        hidePresets={true}
+        hideEyeDrop={false}
+        hideOpacity={true}
+        hideGradientAngle={false}
+        hideGradientControls={false}
+        hideGradientStop={false}
+        hideColorGuide={true}
+        hideInputType={true}
+      />
       
      </div>
         <div className='rounded shadow ejemploGradient' style={{ background: backgroundGradient, minHeight: '70vh', padding: '200px'}}>
