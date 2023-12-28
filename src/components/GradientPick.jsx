facdcb40afc6663bcbbb599abdb377d4ae4c6525
AlexTGarcia020 <129820523+AlexTGarcia020@users.noreply.github.com>
2023-12-28 17:39:22 -0300

    Err V16

--- src/components/GradientPick.jsx
+++ src/components/GradientPick.jsx
@@ -1,42 +1,51 @@
 import React, { useState } from 'react';
-import ColorPicker from 'react-best-gradient-color-picker';
-import "../css/Gradient.css"
+import { ChromePicker } from 'react-color';
+import "../css/Gradient.css";
 
 function GradientPick() {
-  const [backgroundGradient, setBackgroundGradient] = useState('linear-gradient(90deg, rgba(96,93,93,1) 0%, rgba(255,255,255,1) 100%)');
+  const [colorStart, setColorStart] = useState({
+    r: 96,
+    g: 93,
+    b: 93,
+    a: 1,
+  });
 
-  const handleColorChange = (color) => {
-    setBackgroundGradient(color);
+  const [colorEnd, setColorEnd] = useState({
+    r: 255,
+    g: 255,
+    b: 255,
+    a: 1,
+  });
+
+  const handleColorChangeStart = (color) => {
+    setColorStart(color.rgb);
   };
 
-  /* const customLocales = {
-    CONTROLS: {
-      SOLID: 'Solido',
-      GRADIENT: 'Gradiente',
-    },
-  } */
+  const handleColorChangeEnd = (color) => {
+    setColorEnd(color.rgb);
+  };
+
+  const backgroundGradient = `linear-gradient(90deg, rgba(${colorStart.r},${colorStart.g},${colorStart.b},${colorStart.a}) 0%, rgba(${colorEnd.r},${colorEnd.g},${colorEnd.b},${colorEnd.a}) 100%)`;
 
   return (
     <div className='div-Gradient'>
-     <div className='card p-3 rounded shadow'>
-     <ColorPicker
-       /*  locales={customLocales} */
-        value={backgroundGradient}
-        onChange={handleColorChange}
-        hideColorTypeBtns={false}
-        hidePresets={true}
-        hideEyeDrop={false}
-        hideOpacity={true}
-        hideGradientAngle={false}
-        hideGradientControls={false}
-        hideGradientStop={false}
-        hideColorGuide={true}
-        hideInputType={true}
-      />
-      
-     </div>
-        <div className='rounded shadow ejemploGradient' style={{ background: backgroundGradient, minHeight: '70vh', padding: '200px'}}>
-        </div>
+      <div className='card bg-white text-black p-3 rounded shadow'>
+        <ChromePicker
+          color={colorStart}
+          onChange={handleColorChangeStart}
+        />
+        <ChromePicker
+          color={colorEnd}
+          onChange={handleColorChangeEnd}
+          className='mt-2'
+        />
+      </div>
+      <div className='rounded shadow ejemploGradient' style={{
+        background: backgroundGradient,
+        minHeight: '40vh',
+        padding: '200px'
+      }}>
+      </div>
     </div>
   );
 }
